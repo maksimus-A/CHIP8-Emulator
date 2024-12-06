47a35f062ae397c074fd07af1a72babfd4dc0594
Max 🤪 <maksimus@ualberta.ca>
2024-12-06 15:28:35 -0700

    Renders window on Macbook using glfw

--- src/display.c
+++ src/display.c
@@ -1,65 +1,53 @@
-#include<stdio.h>
-#include <stdlib.h>
-#include"SDL.h"
-#include"display.h"
-#include<string.h>
+// Trying to do this on my Mac was a bad idea.
 
+/* Ask for an OpenGL Core Context */
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
 
-display *display_init(unsigned int width, unsigned int height) {
-    display *d = malloc(sizeof(display));
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
-    d->width = width, d->height = height;
+int main(int argc, char** argv)
+{
+  GLFWwindow* window;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window;
-    SDL_Surface* screenSurface;
+  /* Initialize the library */
+  if ( !glfwInit() )
+  {
+     return -1;
+  }
 
-    window = SDL_CreateWindow("Chip8 Emulator",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        (width * PIXEL_SIZE), (height * PIXEL_SIZE), SDL_WINDOW_SHOWN);
+#ifdef __APPLE__
+  /* We need to explicitly ask for a 3.2 context on OS X */
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 
-    if (window == NULL) {
-        fprintf(stderr, "Window could not be created: %s\n", SDL_GetError());
-        return 1;
-    }
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow( 1280, 720, "Hello World", NULL, NULL );
+  if (!window)
+  {
+     glfwTerminate();
+     return -1;
+  }
 
-    screenSurface = SDL_GetWindowSurface(window);
-    
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
 
-    if (!screenSurface) {
-        fprintf(stderr, "Screen surface could not be created: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-    d->screen = screenSurface;
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the buffers
 
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
 
-    return d;
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
-
-void display_free(display* d) {
-    free(d);
-    SDL_Quit();
-}
-
-void display_draw(display* d, uint8_t* buffer) {
-    for (uint8_t Y = 0; Y < d->height; Y++) {
-        for (uint8_t X = 0; X < d->width; X++) {
-            if ((buffer[X + Y * d->width] & 0x01) == 0x01)
-                SDL_SetRenderDrawColor(d->screen, 0xFF, 0xFF, 0xFF, 0xFF); // window, R, G, B, A
-            else
-                SDL_SetRenderDrawColor(d->screen, 0x00, 0x00, 0x00, 0xFF); // window, R, G, B, A
-            SDL_Rect rectangle;
-
-            rectangle.x = X * PIXEL_SIZE;
-            rectangle.y = X * PIXEL_SIZE;
-            rectangle.w = rectangle.x + 1;
-            rectangle.h = rectangle.y + 1;
-            SDL_RenderFillRect(d->screen, &rectangle);
-
-        }
-    }
-    SDL_RenderPresent(d->screen);
-}
-
-
